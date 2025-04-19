2de72d34255fe1dd6cf419fa10d07c677090ff2a
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2025-04-19 12:54:20 +0530

    Create Program-2.cpp

---
+++ Program-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter a: ";
+    cin >> a;
+
+    cout << "Output: ";
+    for (int i = 0; i < a; i++) {
+        cout << (2 * i + 1);
+        if (i != a - 1)
+            cout << ", ";
+    }
+
+    cout << endl;
+    return 0;
+}
